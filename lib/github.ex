b12aa1587e0297cb4e67caf5c0ab4c5b5c4920d6
Yurii Skrynnykov <truef1s7@gmail.com>
2018-04-26 02:51:51 +0300

    basic changes

--- lib/github.ex
+++ lib/github.ex
@@ -1,15 +1,20 @@
 require Logger
 defmodule Github do
 
+  def main(args) do
+    args |> List.first |> dowload_all
+  end
+
   def dowload_all(username) do
     username
     |> repos()
     |> Enum.map(&(Task.start(__MODULE__, :download, [&1])))
   end
 
-  defp download(url) do
+  def download(url) do
     Logger.info "Downloading #{url} repo."
     System.cmd "git", ["clone", url]
+    Logger.info "Finished downloading #{url}."
   end
 
   defp repos(username) do
